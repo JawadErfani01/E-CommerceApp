c2ed2b05a66d5e568560b54560393fa00bec7d86
awad erfani <jawad.erfani147@gmail.com>
2022-01-18 04:22:14 +0430

    Tailwind v3 with reactjs setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className="flex flex-col font-serif justify-center items-center text-white   h-screen bg-slate-800">
+      <h1 className="text-4xl hover:text-blue-600">Frontend With jawad</h1>
+      <a target='_blank' href="https://twitter.com/JawadErfani01"
+       className="py-4 px-10 m-2 transition-all border rounded-full shadow-md   shadow-slate-400 duration-700 ease-linear text-2xl mt-10 hover:bg-slate-500   cursor-pointer">
+       Jawad Erfani
+      </a>
+    </main>
+  );
+}
+
+export default App; 
