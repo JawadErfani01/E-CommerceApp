76900959d435e3ff13ce2f823c860f6572a52a4f
awad erfani <jawad.erfani147@gmail.com>
2022-01-19 04:59:35 +0430

    New fille has been added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <main className="flex flex-col font-serif justify-center items-center text-salte-400   h-screen bg-slate-800">
+    <main className="flex flex-col font-serif justify-center items-center text-slate-400   h-screen bg-slate-800">
       <h1 className="text-4xl hover:text-green-600">Frontend With jawad</h1>
       <a target='_blank' href="https://twitter.com/JawadErfani01"
        className="py-4 px-10 m-2 transition-all border rounded-full shadow-md   shadow-slate-400 duration-700 ease-linear text-2xl mt-10 hover:bg-slate-500   cursor-pointer">
---
+++ src/New.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function New() {
+  return <div>
+      it is new file you added
+  </div>;
+}
+
+export default New;
